aba8d4fc43d1e2fff2ffa5bfa6b2fa7f0a7bc710
Venti <purukansylgwyn@gmail.com>
2026-02-09 23:12:08 +0800

    aiehhh

--- Frontend/src/components/SplashScreen/index.tsx
+++ Frontend/src/components/SplashScreen/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "./style.css";
+import "../../assets/style.css";
 
 const SplashScreen: React.FC = () => {
   const [isVisible, setIsVisible] = useState(true);
