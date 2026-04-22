7415790c3d7536c0fe860069fd6abbb2ba9bb64e
Steveumbas23 <umbasteve10@gmail.com>
2026-04-23 00:06:17 +0800

    update ui

--- Frontend/src/components/Hero/index.tsx
+++ Frontend/src/components/Hero/index.tsx
@@ -26,7 +26,7 @@ function Hero() {
     <main className="hero" id="home">
       <div className="hero-content">
         <h1 className="hero-title">
-          <span className="hero-lung">Liver</span>
+          <span className="hero-lung">Lung</span>
           <span className="hero-and">&amp;</span>
           <span className="hero-colon">Colon</span>
         </h1>
